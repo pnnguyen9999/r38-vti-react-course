9d94e5d0dbbb1cebbef9070876e7a196d6dcafd8
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2023-12-06 21:22:10 +0700

    fix card name

--- src/components/CardClassComponent.jsx
+++ src/components/CardClassComponent.jsx
@@ -7,7 +7,7 @@ export default class CardClassComponent extends Component {
     return (
       <div>
         {this.giaTri1}
-        <h1>Card name: {this.props.name}</h1>
+        <h1>Card name 2: {this.props.name}</h1>
         <h5>Card {this.props.description}</h5>
       </div>
     );
